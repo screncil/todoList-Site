942ea23947ffd1555507d7bc70d10146fc04df11
screncil <alexplay1998s3@gmail.com>
2024-02-24 22:00:14 +0200

    Added logout function and added 'addItem' function

--- todoSite/Home/views.py
+++ todoSite/Home/views.py
@@ -1,10 +1,46 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from Home.models import List
+from Login.models import Users
+
+from .Dependencies import checkFormAddTime
 
 # Create your views here.
 
 
 
 def index(request):
-    return render(request, "Home/index.html")
+    if "session" in request.COOKIES:
+        user = Users.objects.get(session_hash=request.COOKIES['session'])
+        items = List.objects.filter(user_hash=request.COOKIES['session'])
+        return render(request, "Home/index.html", {"user": user, "items": items, "lenitems": len(items)})
+    else:
+        return render(request, "Home/index.html", {"user": None})
+
+
+def logout(request):
+    response = redirect("/")
+    response.delete_cookie("session")
+    return response
+
+
+def addItem(request):
+    if request.method == "POST":
+        data = request.POST.dict()
+        if checkFormAddTime(deadline=data['date']):
+            try:
+                x = List(title=data['title'], description=data['description'], deadline=data['date'], user_hash=request.COOKIES['session'])
+                x.save()
+                return redirect('/')
+            except Exception as ex:
+                print(ex)
+                return redirect('/')
+        else:
+            user = Users.objects.get(session_hash=request.COOKIES['session'])
+            return render(request, "Home/addItem.html", {"user": user, "message": "Время не может быть меньше настоящего"})
+    else:
+        if "session" in request.COOKIES:
+            user = Users.objects.get(session_hash=request.COOKIES['session'])
+            return render(request, "Home/addItem.html", {"user": user, "message": ""})
+        else:
+            return redirect("/")
