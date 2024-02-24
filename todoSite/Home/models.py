d516f0e13974a9876f9c63c1f32ee725a27f4bac
screncil <alexplay1998s3@gmail.com>
2024-02-24 21:56:59 +0200

    Added deadline and user hash

--- todoSite/Home/models.py
+++ todoSite/Home/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class List(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=10000)
-    time_created = models.IntegerField()
+    deadline = models.CharField(max_length=255)
+    user_hash = models.CharField(max_length=255, default="")
 
 
