8e1db9be081960728e6d1ac60c4d83ac970ac49f
screncil <alexplay1998s3@gmail.com>
2024-02-25 23:30:46 +0200

    Added url for delete item

--- todoSite/Home/urls.py
+++ todoSite/Home/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("logout", views.logout, name="logout"),
-    path("additem", views.addItem, name="addItem")
+    path("additem", views.addItem, name="addItem"),
+    path("delitem", views.deleteItem, name="delitem")
 ]
