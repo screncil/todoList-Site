2e705080ca8c6acc21d137d30cb06f1b44459276
screncil <alexplay1998s3@gmail.com>
2024-02-25 23:29:41 +0200

    Added id field

--- todoSite/Home/models.py
+++ todoSite/Home/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 
 class List(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=10000)
     deadline = models.CharField(max_length=255)
     user_hash = models.CharField(max_length=255, default="")
+
+
 
 
